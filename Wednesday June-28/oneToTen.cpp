77539254e1a1bade38060186ea886d76ca2c6eab
infamicstudios <infamicstudios@gmail.com>
2017-07-01 17:18:47 -0700

    Add wensday

---
+++ Wednesday June-28/oneToTen.cpp
@@ -0,0 +1,63 @@
+//Objective: guess a number between 1 and 10.
+//Name: Duncan McFarlane
+//Course: COMSC-110-1021
+//Compiler: TDM MinGW
+//Editor: MS NotePad
+
+//libraries
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+//Programmer defined data types
+//NONE
+
+//Special compiler dependent definitions
+//NONE
+
+//global constants/variables
+//NONE
+
+//Programmer defined functions
+//NONE
+
+//main program
+int main()
+{
+    srand (time(0)); //seed rng
+
+  // output my name and objective and program information
+  cout << "Objective:  guess a number between 1 and 10. \n"; 
+  cout << "Programmer: Duncan McFarlane\n"; 
+  cout << "Editor(s) used: Notepad\n"; 
+  cout << "Compiler(s) used: TDM MinGW\n"; 
+  cout << "File: " << __FILE__ << endl; 
+  cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl << endl; 
+
+  //variables
+  int rng = rand() % 10 + 1;
+  int userinput;
+
+  //intro
+  cout <<"I am thinking of a number between 1 & 10, can you guess the number I am thinking of? \n";
+ 
+ while (userinput != rng) //preforms loop if userinput != the RNG value declared above
+  {
+
+    cout <<"What is your guess [1-10]? \n"; //prompt for guess
+    cin >> userinput;
+    cin.ignore(1000, 10);
+
+    if (userinput > 10 || userinput < 1)
+     {//incorrect input
+       cout <<"I was looking for a number between 1 and 10 \n";
+     }//incorrect input
+
+    cout << "sorry your guess was wrong try again \n";
+
+  }//end the while loop
+
+     cout << "you guessed the right awnser!"; //outputs if the while loop broke
+    
+}//main
